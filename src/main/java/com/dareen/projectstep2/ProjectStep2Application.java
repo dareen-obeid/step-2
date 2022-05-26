c9af729060c652e8fa88e72005f93964725f9bcb
dareen-obeid <201904878@bethlehem.edu>
2022-05-26 13:37:39 +0300

    Here is the commit-msg

---
+++ src/main/java/com/dareen/projectstep2/ProjectStep2Application.java
@@ -0,0 +1,13 @@
+package com.dareen.projectstep2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStep2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStep2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dareen/projectstep2/ProjectStep2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dareen.projectstep2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStep2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
